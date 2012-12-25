35ccfe40fb101d5fef8dc0a96c258223cd81a26e
ahirreddy <ahirreddy@gmail.com>
2012-12-25 13:38:12 -0800

    Added total points

--- fantasy/fantasy/views.py
+++ fantasy/fantasy/views.py
@@ -4,15 +4,16 @@ from django.shortcuts import render
 from django_tables2 import RequestConfig
 import django_tables2 as tables
 
-class AveragePlayerPointsTable(tables.Table):
+class PlayerPointsTable(tables.Table):
   name = tables.Column(verbose_name="Player Name", accessor="player_name")
-  fpts = tables.Column(verbose_name="Avg Fpts", accessor="avg_fpts")
+  avg_fpts = tables.Column(verbose_name="Avg Fpts", accessor="avg_fpts")
+  total_fpts = tables.Column(verbose_name="Total Fpts", accessor="total_fpts")
 
 def index(request):
-  query = """SELECT player_name, round(avg(fpts),2) as avg_fpts
+  query = """SELECT player_name, round(avg(fpts),2) as avg_fpts, sum(fpts) as total_fpts
              FROM fantasy
              GROUP BY player_name
              ORDER BY avg_fpts DESC"""
-  table = AveragePlayerPointsTable(Fantasy.objects.raw(query))
+  table = PlayerPointsTable(Fantasy.objects.raw(query))
   RequestConfig(request).configure(table)
   return render(request, "players.html", {"players": table})
