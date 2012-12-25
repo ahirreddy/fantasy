70fbedb5caf6607d2ea7164d58c3a1b86b7fa950
ahirreddy <ahirreddy@gmail.com>
2012-12-25 13:20:09 -0800

    more working

--- fantasy/fantasy/views.py
+++ fantasy/fantasy/views.py
@@ -1,14 +1,7 @@
 from django.http import HttpResponse
 from fantasy.models import Fantasy
 from django.shortcuts import render
-import django_tables2 as tables
-
-class FantasyTable(tables.Table):
-  class Meta:
-    model = Fantasy
 
 def index(request):
-  qs = Fantasy.objects.raw('SELECT player_name, round(avg(fpts),2) FROM fantasy GROUP BY player_name')
-  table = FantasyTable(qs)
-  tables.RequestConfig(request).configure(table)
-  return render(request, "players.html", {"players": table})
+  qs = Fantasy.objects.raw('SELECT player_name, round(avg(fpts),2) as avg_fpts FROM fantasy GROUP BY player_name ORDER BY avg_fpts DESC')
+  return render(request, "players.html", {"players": qs})
