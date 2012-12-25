84f297240e4e7c5be0475b3ac858251af1d89863
ahirreddy <ahirreddy@gmail.com>
2012-12-25 13:49:47 -0800

    added games played

--- fantasy/fantasy/views.py
+++ fantasy/fantasy/views.py
@@ -8,11 +8,15 @@ class PlayerPointsTable(tables.Table):
   name = tables.Column(verbose_name="Player Name", accessor="player_name")
   avg_fpts = tables.Column(verbose_name="Avg Fpts", accessor="avg_fpts")
   total_fpts = tables.Column(verbose_name="Total Fpts", accessor="total_fpts")
+  games = tables.Column(verbose_name="Games Played", accessor="games_played")
   class Meta:
     attrs = {"class": "paleblue"}
 
 def index(request):
-  query = """SELECT player_name, round(avg(fpts),2) as avg_fpts, sum(fpts) as total_fpts
+  query = """SELECT player_name,
+                    ROUND(AVG(fpts),2) as avg_fpts,
+                    SUM(fpts) as total_fpts,
+                    COUNT(*) as games_played
              FROM fantasy
              GROUP BY player_name
              ORDER BY avg_fpts DESC"""
